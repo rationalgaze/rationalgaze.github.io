2aedf253f7da7f590dfc4e746323acc0417327cd
Nikolai Vorotnikov <nikolai.vorotnikov@altran.com>
2019-11-29 13:31:15 +0100

    upd unuglify

--- js/main.js
+++ js/main.js
@@ -1,1 +1,89 @@
-$(document).ready(function(){var n=function(){var n=0,o=function(){a()},a=function(){s(),$(window).load(l),$(".scroll").click(i),_rehashSubpages()},t=function(){return!!("ontouchstart"in window)||!!("onmsgesturechange"in window)},s=function(){$(window).outerWidth()<1024||t()?$("nav").css("position","absolute"):($(window).on("scroll",e),$(this).scrollTop()>0&&$(window).on("load",e))},e=function(o){o.preventDefault();var a=$(this).scrollTop(),t=$(".navbar").outerHeight();a>n?$("nav").addClass("nav-slideUp").removeClass("nav-slideDown").removeClass("transparent"):($("nav").removeClass("nav-slideUp").addClass("nav-slideDown"),t>=a&&$("nav").addClass("transparent")),n=a},l=function(){$("header h3").addClass("showingUp")},i=function(){event.preventDefault(),url=$(this).attr("href"),console.log(url),console.log(location.pathname),distance=$(url).offset().top,$("html,body").animate({scrollTop:distance},1500),r(url),"none"!=$(".navbar-toggle").css("display")&&$(".navbar-collapse").collapse("hide")},r=function(n){currentHash=window.location.hash,n!=currentHash&&(history.pushState?history.pushState(null,null,n):location.hash=n)};return{init:o}}();n.init()});
+$(document).ready(function() {
+
+  var myApp = (function () {
+
+    var lastScrollTop = 0;
+
+    var init = function(){
+      _setUpListners();
+    };
+
+    var _setUpListners = function () { //прослушка событий
+      _navPosition();
+      $(window).load(_showingUp);
+      $('.scroll').click(_scrollingMenu);
+      _rehashSubpages();
+    };
+
+    var _is_touch_device = function () { // определяем тач устр-во
+          return !!('ontouchstart' in window) || !!('onmsgesturechange' in window);
+      };
+      
+      var _navPosition = function() {
+        if($(window).outerWidth() < 1024 || _is_touch_device())
+      {
+        $('nav').css('position', 'absolute');
+      } 
+      else { 
+        $(window).on('scroll', _nav_float); 
+        if($(this).scrollTop() > 0) $(window).on('load', _nav_float);
+      }
+      };
+
+      var _nav_float = function (e) { //плавающее исчезающее меню
+        e.preventDefault();
+        
+      var y = $(this).scrollTop();
+      var stickyMenuTop = $('.navbar').outerHeight();
+
+      if (y > lastScrollTop ) {
+        $('nav').addClass('nav-slideUp').removeClass('nav-slideDown').removeClass('transparent');
+      } else {
+        $('nav').removeClass('nav-slideUp').addClass('nav-slideDown');
+        if(y <= stickyMenuTop) $('nav').addClass('transparent');
+      }
+
+      lastScrollTop = y;
+    };
+
+    var _showingUp = function() { //всплывающее описание сайта
+      $("header h3").addClass('showingUp');
+    };
+
+      var _scrollingMenu = function() {
+      event.preventDefault();
+          url = $(this).attr('href');
+          console.log(url);
+          console.log(location.pathname);
+          distance = $(url).offset().top;
+          $('html,body').animate({
+              scrollTop: distance
+          }, 1500);
+          _rehash(url);
+          if ($('.navbar-toggle').css('display') != 'none') {
+              $(".navbar-collapse").collapse('hide');
+          }
+
+    };
+  
+    
+    var _rehash = function (url) {
+        currentHash = window.location.hash;
+        if (url != currentHash) {
+            if (history.pushState) {
+                history.pushState(null, null, url);
+            } else {
+                location.hash = url;
+            }
+        }
+      }
+
+    return {
+      init: init
+    };
+
+  })();
+
+  myApp.init();
+
+});
